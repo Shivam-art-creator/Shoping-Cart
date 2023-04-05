f860aaa1115414c7ace90eaec13581f6d6ffe3ce
Shivam kumar <shivamkeshri3580@gmail.com>
2023-04-06 01:04:19 +0530

    second commit

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Product = (props) => {
+    const {id,productName,price,productImage} = props.data;
+  return (
+    <div className='product'>
+    <img src={productImage} />
+      
+    </div>
+  )
+}
+
+
