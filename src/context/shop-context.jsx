6b4d347270584b9dca87f38881c2efdc37ef40db
Shivam kumar <shivamkeshri3580@gmail.com>
2023-04-23 01:20:34 +0530

    Commit

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,39 +1,58 @@
-import React,{createContext} from 'react'
-import { PRODUCTS } from '../products';
-import { useState } from 'react';
-
-// I want to access & change the state because it helps to access and modify these scryRenderedComponentsWithType.
+import { createContext, useEffect, useState } from "react";
+import { PRODUCTS } from "../products";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
-    let cart = {};
-    for(let i=1;i<PRODUCTS.length+1;i++){
-        cart[i]=0;
-    }
-    return cart;
-}
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
 
 export const ShopContextProvider = (props) => {
-    const [cartItems,setCartItems] = useState(getDefaultCart());
- 
-    // setting to change the item which just outering that specific item id and value and adding +1;
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    const addToCart = (itemId) => {
-        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]+1}))
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
     }
+    return totalAmount;
+  };
 
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}))
-    }
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
 
-    const contextValue = {cartItems,addToCart,removeFromCart};
+  const updateCartItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+  };
 
-    // console.log(cartItems);
+  const checkout = () => {
+    setCartItems(getDefaultCart());
+  };
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    updateCartItemCount,
+    removeFromCart,
+    getTotalCartAmount,
+    checkout,
+  };
+
   return (
-   <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
-  )
-}
-
-
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,14 +1,19 @@
-import React,{useContext} from 'react'
-import { PRODUCTS } from '../../products'
-// import { Product } from '../shop/product'
+import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
 
+import "./cart.css";
 export const Cart = () => {
-  const {cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
   return (
-    <div className='cart'>
-    <div>
+    <div className="cart">
+      <div>
         <h1>Your Cart Items</h1>
       </div>
       <div className="cart">
@@ -18,7 +23,24 @@ export const Cart = () => {
           }
         })}
       </div>
-    </div>
-  )
-}
 
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/checkout");
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
+    </div>
+  );
+};
